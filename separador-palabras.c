e6db3a5f70dcfa450492683b349ee430fe7d9f34
Diocleniax789 <julioramdom123@gmail.com>
2024-11-29 20:57:41 -0300

    se agregaron nuevas librerias, el dato tipo struct, y los prototipos

--- separador-palabras.c
+++ separador-palabras.c
@@ -1,5 +1,18 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
+struct pal{
+    char palabra[25];
+    char tipo[20];
+};
+
+int carga_reserva(char[40][15]);
+char * carga_texto(void);
+struct pal * separa(char*,int*);
+struct pal * depura(char*,int*);
+void clasifica(struct pal*,char pal_res[][15],int);
 
 int main(){
 
