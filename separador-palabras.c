ca48665212c841400299e7530a77608110395209
Diocleniax789 <julioramdom123@gmail.com>
2024-11-29 21:12:42 -0300

    se creo el programa principal con sus respectivas funciones invocadas

--- separador-palabras.c
+++ separador-palabras.c
@@ -11,14 +11,21 @@ struct pal{
 int carga_reserva(char[40][15]);
 char * carga_texto(void);
 struct pal * separa(char*,int*);
-struct pal * depura(char*,int*);
+struct pal * depura(struct pal*,int*);
 void clasifica(struct pal*,char pal_res[][15],int);
 
 int main(){
+    int cant_pal_res,palabras_cargadas = 0;
+    char pal_res[40][15],*texto;
+    struct pal * todas_las_palabras;
+    struct pal * palabras_depuradas;
 
-
-
+    cant_pal_res = carga_reserva(pal_res);
+    texto = carga_texto();
+    todas_las_palabras = separa(texto,&palabras_cargadas);
+    palabras_depuradas = depura(todas_las_palabras,palabras_cargadas);
+    clasifica(palabras_depuradas,pal_res,cant_pal_res);
 
     return 0;
-
 }
+
